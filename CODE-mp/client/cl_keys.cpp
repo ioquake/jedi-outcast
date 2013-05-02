2647f1e8d1807216c2e276e96ffe230fee429a38
Jonathan Gray <jsg@jsg.id.au>
2013-05-02 17:21:31 +1000

    const fixes

--- CODE-mp/client/cl_keys.cpp
+++ CODE-mp/client/cl_keys.cpp
@@ -721,7 +721,7 @@ static void keyConcatArgs( void ) {
 }
 
 static void ConcatRemaining( const char *src, const char *start ) {
-	char *str;
+	const char *str;
 
 	str = strstr(src, start);
 	if (!str) {
@@ -997,7 +997,7 @@ the K_* names are matched up.
 to be configured even if they don't have defined names.
 ===================
 */
-int Key_StringToKeynum( char *str ) {
+int Key_StringToKeynum( const char *str ) {
 	int			i;
 	
 	if ( !str || !str[0] ) 
